0e31a647c1e5fbabcdc30843cebfbba50a315047
Matthew Clarke <matthewaclarke1991@gmail.com>
2025-07-26 18:02:18 +0100

    mocker

--- tests/testthat/test-run_autopert_config.R
+++ tests/testthat/test-run_autopert_config.R
@@ -1,26 +1,24 @@
 source(here::here("tests", "testthat", "testing_utils.r"))
 
+library(mockery)
+
 test_that("run_autopert_config.r works with valid config", {
   skip_if_not(Sys.info()[["sysname"]] == "Windows", "autopert requires Windows BMA tools")
 
-  # Save original commandArgs
-  old_commandArgs <- commandArgs
-
-  # Set up mock commandArgs like temp_test_command_line.r
   args <- c(here::here("examples/run_autopert_config.r"), here::here("examples/autopert_config_example.json"))
-  commandArgs <- function(trailingOnly = FALSE) {
-    if (trailingOnly) {
-      return(args[-1])  # Remove script name
-    } else {
-      return(c("R", "--slave", "--no-restore", "--file=script.R", "--args", args[-1]))
+
+  with_mock(
+    commandArgs = function(trailingOnly = FALSE) {
+      if (trailingOnly) {
+        return(args[-1])
+      } else {
+        return(c("R", "--slave", "--no-restore", "--file=script.R", "--args", args[-1]))
+      }
+    },
+    {
+      expect_no_error(source(here::here("examples/run_autopert_config.r")))
     }
-  }
-
-  # Restore on exit
-  on.exit(commandArgs <- old_commandArgs)
-
-  # Test that script runs without error
-  expect_no_error(source(here::here("examples/run_autopert_config.r")))
+  )
 })
 
 test_that("run_autopert_config.r handles no arguments", {
